97c2159cc67afe85027adf055de1300c3c615961
Lucas <lucasf854@gmail.com>
2020-08-19 14:40:47 -0300

    contact modal

---
+++ app.js
@@ -0,0 +1,11 @@
+const contactBtn = document.querySelector("#contactBtn") 
+const contactBg = document.querySelector("#contact-bg")
+
+// console.log(contactBg)
+contactBtn.addEventListener('click',()=>{
+    contactBg.style['visibility'] = 'visible' 
+})
+
+// contactBg.addEventListener('click',()=>{
+//     contactBg.style['visibility'] = 'hidden' 
+// })
