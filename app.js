7bc7c988afe34191175dc95dc617e85da3bbfd4c
Lucas <lucasf854@gmail.com>
2020-08-20 10:59:07 -0300

    contact links

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const contactBtn = document.querySelector("#contactBtn") 
 const contactBg = document.querySelector("#contact-bg")
+const closeBtn = document.querySelector('.close')
 
 // console.log(contactBg)
 contactBtn.addEventListener('click',()=>{
     contactBg.style['visibility'] = 'visible' 
+})
+
+closeBtn.addEventListener('click',()=>{
+    contactBg.style['visibility'] = 'hidden' 
 })
 
 // contactBg.addEventListener('click',()=>{
