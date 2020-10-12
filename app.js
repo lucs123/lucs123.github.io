3f0b0f49c09fa6394bff88ca2710be6d2c5d2b72
Lucas <lucasf854@gmail.com>
2020-10-12 13:05:05 -0300

    pt translation

--- app.js
+++ app.js
@@ -25,12 +25,19 @@ const typewriter = new Typewriter(presentationWrapper, {
 let count = 0;
 function updateContent() {
   document.getElementById('contactBtn').innerHTML = i18next.t('headerLinks.contact');
+  document.getElementById('greetingWrapper').innerHTML = i18next.t('greeting');
+
   document.getElementById('project1Link').innerHTML = i18next.t('projectLinks.project1');
   document.getElementById('project2Link').innerHTML = i18next.t('projectLinks.project2');
   document.getElementById('project3Link').innerHTML = i18next.t('projectLinks.project3');
-    type(i18next.t('presentationText'))
-    console.log(count)
-    count += 1
+  type(i18next.t('presentationText'))
+
+  document.getElementById('project1Presentation').innerHTML = i18next.t('projectPresentation1');
+  document.getElementById('project2Presentation').innerHTML = i18next.t('projectPresentation2');
+  document.getElementById('project3Presentation').innerHTML = i18next.t('projectPresentation3');
+
+  document.getElementById('project3Title').innerHTML = i18next.t('project3Title');
+
   // document.getElementById('info').innerHTML = `detected user language: "${i18next.language}"  --> loaded languages: "${i18next.languages.join(', ')}"`;
 }
 
